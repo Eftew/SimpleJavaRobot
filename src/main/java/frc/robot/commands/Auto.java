6047148aac0e5e0afe6ce091fde0dc1fdd33800e
Flippette <quangdatle2006@outlook.com>
2021-11-27 10:37:56 +0700

    Auto should be parallel

--- src/main/java/frc/robot/commands/Auto.java
+++ src/main/java/frc/robot/commands/Auto.java
@@ -5,7 +5,7 @@
 package frc.robot.commands;
 
 // WPILib libraries
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 
 // Subsystems
 import frc.robot.subsystems.*;
@@ -17,7 +17,7 @@ import static frc.robot.Constants.Time;
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class Auto extends SequentialCommandGroup {
+public class Auto extends ParallelCommandGroup {
   /** Creates a new Auto. */
   public Auto(DriveBase driveBase, Sucker sucker, Shooter shooter) {
     // Add your commands in the addCommands() call, e.g.
